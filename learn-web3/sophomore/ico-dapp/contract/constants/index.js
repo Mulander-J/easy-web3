dcd9ff0c0f3f90a5ea696a3304fda09a1695fd01
Mulander <39454158+Mulander-J@users.noreply.github.com>
2022-09-26 14:55:24 +0800

    feat: sophomore.ico

---
+++ learn-web3/sophomore/ico-dapp/contract/constants/index.js
@@ -0,0 +1,3 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x98624Ace032b6006Abb73243464DCb3bCC850E4F";
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
